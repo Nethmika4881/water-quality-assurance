9c4384668fd9f9541d59ebe626f3908515bbc69d
Nethmika4881 <nethmikakumarasiri@gmail.com>
2026-01-21 03:19:54 +0530

    g

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
